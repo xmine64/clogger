055c79d2ddb91c3128eed95af25df046f2424a15
Mohammad Amin Mollazadeh <madmanda2020@gmail.com>
2020-05-09 13:37:12 +0430

    add time support to test

--- test.c
+++ test.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <time.h>
 
 #include <clogger.h>
 
 int main() {
+	double usage_clock = (double)clock();
+
 	clog_logf("Hello World!");
 
 	for (int i = 1000; i < 1010; i++)
@@ -19,4 +22,7 @@ int main() {
 	clog_free();
 
 	assert(clog_count() == 0);
+
+	usage_clock = ((double)clock() - usage_clock)/CLOCKS_PER_SEC;
+	printf("used cpu clock: %f miliseconds\r\n", usage_clock*1000);
 }
